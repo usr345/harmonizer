f0a8de25eee3526251110717ac6b9451b8499987
Ivanov Dmitriy <usr345@gmail.com>
2022-04-20 21:56:58 +0300

    tests.pl improved

--- tests.pl
+++ tests.pl
@@ -10,94 +10,73 @@
 :- discontiguous(test/4).
 :- discontiguous(test_harm/1).
 
+run_test(Test) :- test(Test, Params, Pred/_, positive),
+                  apply(Pred, Params).
+
+run_test(Test) :- test(Test, Params, Pred/_, negative),
+                  \+ apply(Pred, Params).
+
+run_test(Test) :- test(Test, Params, Pred/_, unique),
+                  append(First, [Val], Params),
+                  % вызываем Pred со всеми параметрами, кроме последнего
+                  % в него записываем переменную X
+                  % Потом сравниваем, что результат в X равен Val.
+                  findall(X, (append(First, [X], P), apply(Pred, P)), [Val]).
+
+run_test_pred(Pred) :-
+    forall(test(Test, _, Pred/_, _), run_test(Test)).
+
 test(test1, [note(1, 5), note(3, 5)], stage_less/2, positive).
 test(test2, [note(1, 5), note(1, 6)], stage_less/2, positive).
-
-%% run_test(Test) :- test(Test, Params, Pred/2, positive),
-%%                   apply(Pred, Params).
-
-run_test(Test) :- test(Test, [N1, N2], stage_less/2, positive),
-                       stage_less(N1, N2).
-
 test(test3, [note(2, 5), note(1, 5)], stage_less/2, negative).
 test(test4, [note(1, 5), note(1, 5)], stage_less/2, negative).
-
-run_test(Test) :- test(Test, [N1, N2], stage_less/2, negative),
-                  \+ stage_less(N1, N2).
-
-
 test(test5, [note(1, 5), note(3, 5)], stage_le/2, positive).
 test(test6, [note(1, 5), note(1, 6)], stage_le/2, positive).
 test(test7, [note(1, 6), note(1, 6)], stage_le/2, positive).
-
-run_test(Test) :- test(Test, [N1, N2], stage_le/2, positive),
-                  stage_le(N1, N2).
-
 test(test8, [note(3, 5), note(1, 2)], stage_le/2, negative).
 
-run_test(Test) :- test(Test, [N1, N2], stage_le/2, negative),
-                  \+ stage_less(N1, N2).
-
 % notes_cmp
-test(test9, [note(1, 1), note(1, 3), -1], notes_cmp, positive).
-test(test10, [note(2, 3), note(2, 3), 0], notes_cmp, positive).
-test(test11, [note(2, 5), note(2, 3), 1], notes_cmp, positive).
-
-run_test(Test) :- test(Test, [N1, N2, Val], notes_cmp, positive),
-                  findall(X, notes_cmp(N1, N2, X), [Val]).
+test(test9, [note(1, 1), note(1, 3), -1], notes_cmp/3, unique).
+test(test10, [note(2, 5), note(2, 3), 1], notes_cmp/3, unique).
+test(test11, [note(3, 3), note(5, 3), -1], notes_cmp/3, unique).
+test(test23, [note(3, 2), note(3, 2), 0], notes_cmp/3, unique).
+test(test24, [note(3, 5), note(1, 5), 1], notes_cmp/3, unique).
 
 % less_then_oct
 test(test12, [note(5, 3), note(4, 3)], less_then_oct/2, positive).
 test(test13, [note(2, 3), note(2, 3)], less_then_oct/2, positive).
 test(test14, [note(5, 3), note(5, 2)], less_then_oct/2, positive).
-
-run_test(Test) :- test(Test, [N1, N2], less_then_oct/2, positive),
-                  less_then_oct(N1, N2).
-
 test(test15, [note(5, 3), note(4, 2)], less_then_oct/2, negative).
 test(test16, [note(5, 3), note(2, 2)], less_then_oct/2, negative).
-
-run_test(Test) :- test(Test, [N1, N2], less_then_oct/2, negative),
-                  \+ less_then_oct(N1, N2).
-
 
 % задается нота 1, и по ней ищется нота 2, которая лежит ниже
 test(test17, [note(2, 1), note(1, 5)], nearest_down/2, positive).
 test(test18, [note(2, 3), note(1, 3)], nearest_down/2, positive).
 test(test19, [note(2, 5), note(2, 3)], nearest_down/2, positive).
-run_test(Test) :- test(Test, [N1, N2], nearest_down/2, positive),
-                  findall(X, nearest_down(N1, X), [N2]).
-
 
 % нота, [список разрешенных октав], ступень
 test(test20, [note(5, 3), [5, 4], 1], nearest_down_bass/2, positive).
 test(test21, [note(5, 3), [5, 4, 3], 3], nearest_down_bass/2, positive).
 
 same_elements([], []).
-same_elements([X | XS], Y) :- append([A, [X], B], Y),
+same_elements([X | XS], Y) :- append([A, [X], B], Y), % в Y-е встречается X?
                               append(A, B, Z),
                               same_elements(XS, Z), !.
 
-%% same_elements([a, b, c, a], [c, a, b, a]).
-%% same_elements([a, b, c, a], [c, a, b]).
-%% same_elements([a, b, c, a], [c, a, a, b]).
+test(test22, [[a, b, c, a], [c, a, b, a]], same_elements/2, positive).
+test(test22_1, [[a, b, c, a], [c, a, a, b]], same_elements/2, positive).
+test(test22_2, [[], []], same_elements/2, positive).
+test(test22_3, [[a, b, c, a], [c, a, b]], same_elements/2, negative).
+test(test22_4, [[a, b, c, a, d], [c, a, a, b]], same_elements/2, negative).
+test(test22_5, [[a], []], same_elements/2, negative).
+
 
 run_test(Test) :- test(Test, [N, Octs, Stage], nearest_down_bass/2, positive),
                   findall(X, nearest_down_bass(N, note(X, Stage)), R),
                   same_elements(R, Octs).
 
-test(test22, [note(3, 2), note(5, 3), -1], notes_cmp/3, positive).
-test(test23, [note(3, 2), note(3, 2), 0], notes_cmp/3, positive).
-test(test24, [note(3, 5), note(3, 2), 1], notes_cmp/3, positive).
-
-run_test(Test) :- test(Test, [N1, N2, Val], notes_cmp/3, positive),
-                  findall(X, notes_cmp(N1, N2, X), [Val]).
-
 test(test25, [7, [stage(0,1), stage(2,2), stage(4, 3), stage(5, 4), stage(7, 5), stage(9, 6), stage(11, 7)], 12, note(0, 4)], altitude2note/4, positive).
 test(test26, [7, [stage(0,1), stage(2,2), stage(4, 3), stage(5, 4), stage(7, 5), stage(9, 6), stage(11, 7)], 6, note(-1, 7)], altitude2note/4, positive).
-
-run_test(Test) :- test(Test, [S, T, A, Val], altitude2note/4, positive),
-                  findall(X, altitude2note(S, T, A, X), [Val]).
 
 % Предикаты, на которые есть тесты
 predicates_tested(Module, Set) :- findall(P, (module_property(Module, exports(X)), member(P, X), test(_, _, P, _)), List),
@@ -106,23 +85,11 @@ predicates_tested(Module, Set) :- findall(P, (module_property(Module, exports(X)
 predicates_not_tested(Module, Set) :- findall(P, (module_property(Module, exports(X)), member(P, X), \+ test(_, _, P, _)), List),
                                   list_to_set(List, Set).
 
+% Запрет на перенос одного типа аккорда со слабой доли на сильную
 test(test27, [[ta, sa, da, da, ta, da, ta, ta], [4, 2, 3, 1, 4, 2, 3, 1]], check_downbeat/2, positive).
 test(test28, [[ta, ta, ta, ta, ta, ta, ta, ta], [4, 2, 3, 1, 4, 2, 3, 1]], check_downbeat/2, positive).
-
-run_test(Test) :- test(Test, [T, Strengths], check_downbeat/2, positive),
-                  check_downbeat(T, Strengths).
-
-test(test27, [[ta, sa, da, da, ta, da, ta, ta], [4, 2, 3, 1, 4, 2, 3, 1]], check_downbeat/2, positive).
-test(test28, [[ta, ta, ta, ta, ta, ta, ta, ta], [4, 2, 3, 1, 4, 2, 3, 1]], check_downbeat/2, positive).
-
-run_test(Test) :- test(Test, [T, Strengths], check_downbeat/2, positive),
-                  check_downbeat(T, Strengths).
-
 test(test29, [[ta, sa, sa, da, ta, da, ta, ta], [4, 2, 3, 1, 4, 2, 3, 1]], check_downbeat/2, negative).
 test(test30, [[ta, sa, da, da, da, ta, ta, ta], [4, 2, 3, 1, 4, 2, 3, 1]], check_downbeat/2, negative).
-
-run_test(Test) :- test(Test, [T, Strengths], check_downbeat/2, negative),
-                  \+ check_downbeat(T, Strengths).
 
 %% nearest_down_bass
 
